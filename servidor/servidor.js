441d019acc2e11a4f3e7bc992176c79223b039b3
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2025-05-20 13:31:04 -0300

    update

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -27,6 +27,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 var dbo = client.db("helloworld")
 var usuarios = dbo.collection("usuarios")
 var posts = dbo.collection("posts")
+var clientes = dbo.collection("clientes")
+var carros = dbo.collection("carros")
 
 app.get("/cadastrar", function(requisicao, resposta){
     let nome = requisicao.query.nome;
@@ -110,3 +112,153 @@ app.get("/listar_posts", function(req, resp) {
       });
 
 });
+
+//////LAB 10
+
+app.get("/cadastrar_usuario", function(requisicao, resposta){
+    let nome = requisicao.query.nome;
+    let login = requisicao.query.login;
+    let senha = requisicao.query.senha;
+
+    console.log(nome, login, senha)
+})
+
+app.post("/cadastrar _usuario", function(requisicao, resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    console.log(nome, login, senha);
+
+    var data = { db_nome: nome, db_login: login, db_senha: senha,};
+
+    clientes.insertOne(data, function(err){
+        if(err){
+            resposta.render("resposta",{status: "Erro" ,nome, login, senha});
+        }else{
+            resposta.render("resposta",{status: "Sucesso", nome, login, senha});
+        }
+    })
+})
+
+app.get("/logar_usuario", function(requisicao, resposta){
+    let nome = requisicao.query.nome;
+    let login = requisicao.query.login;
+    let senha = requisicao.query.senha;
+    resposta.render("resposta",{nome, login, senha})
+})
+
+app.post('/logar_usuario', function(requisicao, resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;  
+    console.log(nome, login, senha);
+
+    var data = { db_nome: nome, db_login: login, db_senha: senha,}
+
+    clientes.find(data).toArray(function(err, items){
+        console.log(items)
+        if(items.length == 0){
+            resposta.render("resposta_login",{status: "usuario/senha não encontrado"});
+        }else if(err){
+            resposta.render("resposta_login",{status: "erro ao logar"});
+        }else{
+            resposta.render("resposta_login",{status: "usuario "+login+" logado"});
+        }
+    })
+})
+
+app.get("/cadastrar_carro", function(requisicao, resposta){
+    let marca = requisicao.query.nome;
+    let modelo = requisicao.query.login;
+    let ano = requisicao.query.senha;
+    let qtd_disponivel = requisicao.query.nascimento;
+
+    console.log(nome, login, senha, nasc)
+})
+
+app.post("/cadastrar", function(requisicao, resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nascimento;
+    console.log(nome, login, senha, nasc);
+
+    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+
+    usuarios.insertOne(data, function(err){
+        if(err){
+            resposta.render("resposta",{status: "Erro" ,nome, login, senha, nasc});
+        }else{
+            resposta.render("resposta",{status: "Sucesso", nome, login, senha, nasc});
+        }
+    })
+})
+
+app.get("/for_ejs",function(requisicao, resposta){
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for",{valor});
+})
+
+app.post('/logar', function(requisicao, resposta){
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    console.log(login, senha);
+
+    var data = {db_login: login, db_senha: senha}
+
+    usuarios.find(data).toArray(function(err, items){
+        console.log(items)
+        if(items.length == 0){
+            resposta.render("resposta_login",{status: "usuario/senha não encontrado"});
+        }else if(err){
+            resposta.render("resposta_login",{status: "erro ao logar"});
+        }else{
+            resposta.render("resposta_login",{status: "usuario "+login+" logado"});
+        }
+    })
+
+})
+
+
+app.post('/atualizar_senha', function(requisicao, resposta){
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let novasenha = requisicao.body.novasenha;
+
+    let data = { db_login: login, db_senha: senha }
+    let new_data = { $set: {db_senha: novasenha}}
+
+    usuarios.updateOne(data, new_data, function(err, result){
+        console.log(result);
+    
+        if (result.modifiedCount == 0) {
+            resposta.render('resposta_login', {status: "Usuário/senha não encontrado!"})
+        }else if (err) {
+            resposta.render('resposta_login', {status: "Erro ao atualizar usuário!"})
+        }else {
+            resposta.render('resposta_login', {status: "Usuário atualizado com sucesso!"})        
+        };
+    })
+
+})
+
+
+
+app.post('/remover_usuario', function(requisicao, resposta){
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+
+    let data = { db_login: login, db_senha: senha }
+
+    usuarios.deleteOne(data, function(err, result){
+        console.log(result);
+
+          if (result.deletedCount == 0) {
+            resposta.render('resposta_login', {status: "Usuário/senha não encontrado!"})
+          }else if (err) {
+            resposta.render('resposta_login', {status: "Erro ao remover usuário!"})
+          }else {
+            resposta.render('resposta_login', {status: "Usuário removido com sucesso!"})        
+          };
+    })
+})
